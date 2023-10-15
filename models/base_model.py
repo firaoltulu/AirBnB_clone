e364eaa86de54fe7e4ae4e4b725999cbfed63c19
firaoltulu <firaoltulu5@gmail.com>
2023-10-15 14:57:28 -0700

    my third commit

--- models/base_model.py
+++ models/base_model.py
@@ -24,10 +24,10 @@ class BaseModel:
             for one in kwargs:
                 if one == "created_at":
                     self.__dict__["created_at"] = datetime.strptime(
-                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                            kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 elif one == "updated_at":
                     self.__dict__["updated_at"] = datetime.strptime(
-                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                            kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 else:
                     self.__dict__[one] = kwargs[one]
         else:
@@ -41,7 +41,7 @@ class BaseModel:
         of an instance."""
 
         return "[{}] ({}) {}".\
-            format(type(self).__name__, self.id, self.__dict__)
+                format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
         """This function Updates the updated_at attribute
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -36,12 +36,12 @@ class FileStorage:
         from models.review import Review
 
         classes = {"BaseModel": BaseModel,
-                   "User": User,
-                   "State": State,
-                   "City": City,
-                   "Amenity": Amenity,
-                   "Place": Place,
-                   "Review": Review}
+                "User": User,
+                "State": State,
+                "City": City,
+                "Amenity": Amenity,
+                "Place": Place,
+                "Review": Review}
         return classes
 
     def reload(self):
@@ -51,44 +51,44 @@ class FileStorage:
         with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
             obj_dict = json.load(f)
             obj_dict = {k: self.classes()[v["__class__"]](**v)
-                        for k, v in obj_dict.items()}
+                    for k, v in obj_dict.items()}
             # TODO: should this overwrite or insert?
             FileStorage.__objects = obj_dict
 
     def attributes(self):
         """This method Returns the valid attributes and their types for classname."""
         attributes = {
-            "BaseModel":
-                     {"id": str,
-                      "created_at": datetime.datetime,
-                      "updated_at": datetime.datetime},
-            "User":
-                     {"email": str,
-                      "password": str,
-                      "first_name": str,
-                      "last_name": str},
-            "State":
-                     {"name": str},
-            "City":
-                     {"state_id": str,
-                      "name": str},
-            "Amenity":
-                     {"name": str},
-            "Place":
-                     {"city_id": str,
-                      "user_id": str,
-                      "name": str,
-                      "description": str,
-                      "number_rooms": int,
-                      "number_bathrooms": int,
-                      "max_guest": int,
-                      "price_by_night": int,
-                      "latitude": float,
-                      "longitude": float,
-                      "amenity_ids": list},
-            "Review":
-            {"place_id": str,
-                         "user_id": str,
-                         "text": str}
-        }
+                "BaseModel":
+                {"id": str,
+                    "created_at": datetime.datetime,
+                    "updated_at": datetime.datetime},
+                "User":
+                {"email": str,
+                    "password": str,
+                    "first_name": str,
+                    "last_name": str},
+                "State":
+                {"name": str},
+                "City":
+                {"state_id": str,
+                    "name": str},
+                "Amenity":
+                {"name": str},
+                "Place":
+                {"city_id": str,
+                    "user_id": str,
+                    "name": str,
+                    "description": str,
+                    "number_rooms": int,
+                    "number_bathrooms": int,
+                    "max_guest": int,
+                    "price_by_night": int,
+                    "latitude": float,
+                    "longitude": float,
+                    "amenity_ids": list},
+                "Review":
+                {"place_id": str,
+                    "user_id": str,
+                    "text": str}
+                }
         return attributes
